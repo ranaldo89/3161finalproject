8e1cdcf251b97498eeee0c9b464042f8e7077698
Ranaldo Campbell <ranaldo89@gmail.com>
2020-05-03 15:19:43 -0500

    create database

---
+++ mybook.sql
@@ -0,0 +1,14 @@
+create database mybook;
+
+use mybook;
+
+create table users(
+	userid varchar(100),
+	username varchar(100),
+	password varchar(100),
+	firstname varchar(100),
+	lastname varchar(100),
+	gender varchar(100),
+	email varchar(100),
+	dob varchar(100)
+);
