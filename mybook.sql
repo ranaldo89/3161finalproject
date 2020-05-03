e7b9a1b7368dc199a2bcf6d30ced1fdd113ac13b
Ranaldo Campbell <ranaldo89@gmail.com>
2020-05-03 15:48:41 -0500

    create tables

--- mybook.sql
+++ mybook.sql
@@ -11,4 +11,31 @@ create table users(
 	gender varchar(100),
 	email varchar(100),
 	dob varchar(100)
+	primary key (userid)
 );
+
+
+create table profiles(
+	profile_id varchar(100),
+	city varchar(100),
+	school varchar(100),
+	job varchar(100),
+	numberoffriends varchar(100),
+	numberofposts varchar(100),
+	primary key (profile_id)
+);
+
+create table groups(
+	group_id varchar(100),
+	groupname varchar(100),
+
+	primary key (group_id)
+);
+
+create table posts(
+	post_id varchar(100),
+	post_type varchar(100),
+
+	primary key (post_id)
+);
+
